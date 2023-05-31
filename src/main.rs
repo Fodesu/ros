ee73f10c0d57061ef67a61be96bf3540160b3e58
HimenoSenaa <himeno.senaa@gmail.com>
2023-05-31 10:35:14 +0800

    ch3 VGA Text

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 #![no_std]
 #![no_main]
+
+mod vga_buffer;
 use core::panic::PanicInfo;
+
+static HELLO : &[u8] = b"Hello World!";
 
 
 #[panic_handler]
-fn panic(_info : &PanicInfo) ->! {
+fn panic(info : &PanicInfo) ->! {
+    println!("{}", info);
     loop {}
 }
-
 #[no_mangle]
-pub extern "C" fn _start() -> ! { // ! 表示该函数没有返回
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    
     loop {}
 }
