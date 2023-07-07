b1e32549a71abd3786ecb5eeb16e50731dd8dc38
HimenoSenaa <himeno.senaa@gmail.com>
2023-07-08 06:59:14 +0800

    before async

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,97 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(ros::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
+extern crate alloc;
+
+use bootloader::{BootInfo, entry_point};
+use ros::{println, memory};
 use core::panic::PanicInfo;
+use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
+use ros::task::{Task, simple_executor::SimpleExecutor};
 
-static HELLO : &[u8] = b"Hello World!";
+entry_point!(kernel_main);
 
-
-#[panic_handler]
-fn panic(info : &PanicInfo) ->! {
-    println!("{}", info);
-    loop {}
-}
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
+fn kernel_main(boot_info : &'static BootInfo) ->! {
     
-    loop {}
+    use x86_64::{VirtAddr}; // 新的导入
+    use ros::memory::BootInfoFrameAllocator;
+    use ros::allocator;
+    println!("Hello World{}", "!");
+    ros::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+
+
+    
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+    .expect("heap initialization failed");
+
+
+
+    let mut executor = SimpleExecutor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.run();
+
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
+
+
+
+    // as before
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    ros::hlt_loop();
+}
+
+
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    ros::hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    ros::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -157,5 +157,42 @@ macro_rules! println {
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
-    WRITER.lock().write_fmt(args).unwrap();
+    use x86_64::instructions::interrupts;
+    interrupts::without_interrupts(|| {
+        WRITER.lock().write_fmt(args).unwrap();
+    });
+    
+}
+
+
+
+
+
+
+#[test_case]
+fn test_println_simply() {
+    println!("test_println_simply output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for i in 0..200 {
+        println!("test_println_simply output {}", i);
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    use x86_64::instructions::interrupts;
+    use core::fmt::Write;
+    let s = "Some test string that fits on a single line";
+    interrupts::without_interrupts(||{
+        let mut writer = WRITER.lock();
+        writeln!(writer, "\n{}", s).expect("writeln failed");
+        for (i, c) in s.chars().enumerate() {
+            let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+            assert_eq!(char::from(screen_char.ascii_character), c);
+        }
+    });
+
 }
